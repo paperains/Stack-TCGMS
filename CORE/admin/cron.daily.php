5fbf476037d8d1991b8879460e6406c53976e006
Aki <76135687+paperains@users.noreply.github.com>
2021-02-16 13:09:36 +0800

    Added query to run any scheduled posts.

--- CORE/admin/cron.daily.php
+++ CORE/admin/cron.daily.php
@@ -6,4 +6,9 @@ $sanitize = new Sanitize;
 
 // SET USER TO INACTIVE AFTER XX DAYS (default is 60 days)
 $database->query("UPDATE `user_list` SET `status`='Inactive' WHERE `session` < CURDATE() - INTERVAL 60 DAY");
+
+// PUBLISH ANY SCHEDULED POSTS
+$count = $database->num_rows("SELECT *,DATE(timestamp) FROM `tcg_blog` WHERE DATE(`timestamp`) = CURDATE()");
+$sql = $database->get_assoc("SELECT *,DATE(timestamp) FROM `tcg_blog` WHERE DATE(`timestamp`) = CURDATE()");
+if ($count != 0) { $database->query("UPDATE `tcg_blog` SET `status`='Published' WHERE `id` = '".$sql['id']."'"); }
 ?>
