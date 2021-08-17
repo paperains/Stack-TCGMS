db6d3953fa803bec95eb7c8870cb95d606e5953b
Aki <76135687+paperains@users.noreply.github.com>
2021-08-18 00:43:06 +0800

    Delete CORE directory

--- CORE/admin/games/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Silence is golden...
-// But why is duct tape silver?
-// Electrical tape is black though.
--- CORE/admin/theme/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Silence is golden...
-// But why duct tape is silver?
-// Electrical tape is black though.
